331396724b8c28950b4c965a8be90060a5e17314
zhuhuihui <jackjboss@gmail.com>
2017-06-09 23:39:00 +0800

    avro 文件封装

--- src/main/java/com/chsoft/datacollection/entity/Video.java
+++ src/main/java/com/chsoft/datacollection/entity/Video.java
@@ -2,4 +2,41 @@ package com.chsoft.datacollection.entity;
 
 public class Video {
 
+	private int id;
+	private String videoName;
+	private String desc;
+	private int status;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getVideoName() {
+		return videoName;
+	}
+
+	public void setVideoName(String videoName) {
+		this.videoName = videoName;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
 }
